a42f75a3ecc1847b043ede07b232cbba8fb9c9b4
Gustavo Paulo <gustavo.psilvaa02@gmail.com>
2024-05-17 19:21:09 -0300

    feat: criação da anotação Adapter

--- src/main/java/com/gustavo/adapters/persistence/repository/jpa/PropostaJpaRepository.java
+++
@@ -1,7 +0,0 @@
-package com.gustavo.adapters.persistence.repository.jpa;
-
-import com.gustavo.adapters.persistence.model.PropostaEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PropostaJpaRepository extends JpaRepository<PropostaEntity, Long> {
-}
