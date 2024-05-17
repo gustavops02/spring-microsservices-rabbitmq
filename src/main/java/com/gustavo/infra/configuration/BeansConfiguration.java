965e4ac75d218c3f44afcc4f0137b24d1df0e71a
Gustavo Paulo <gustavo.psilvaa02@gmail.com>
2024-05-17 19:24:31 -0300

    config: criação do Bean para a nossa porta de entrada que é chamada no Controller

--- src/main/java/com/gustavo/infra/configuration/BeansConfiguration.java
+++ src/main/java/com/gustavo/infra/configuration/BeansConfiguration.java
@@ -1,8 +1,17 @@
 package com.gustavo.infra.configuration;
 
-
+import com.gustavo.domain.ports.in.PropostaServicePort;
+import com.gustavo.domain.ports.out.PropostaRepositoryPort;
+import com.gustavo.domain.services.PropostaService;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class BeansConfiguration {
+
+    @Bean
+    public PropostaServicePort propostaServicePort(PropostaRepositoryPort repositoryPort) {
+        return new PropostaService(repositoryPort);
+
+    }
 }
