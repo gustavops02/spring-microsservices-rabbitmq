a71fc6a5980839d99f0d45df3cf71bbf1c880573
Gustavo Paulo <gustavo.psilvaa02@gmail.com>
2024-05-17 17:50:11 -0300

    feat: criação das entidades Proposta e Usuario

---
+++ src/main/java/com/gustavo/adapters/persistence/model/UsuarioEntity.java
@@ -0,0 +1,36 @@
+package com.gustavo.adapters.persistence.model;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(of = "id")
+@Entity
+@Table(name = "tb_usuario")
+public class UsuarioEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String sobrenome;
+    private String cpf;
+    private String telefone;
+    private Double renda;
+
+    @OneToOne(mappedBy = "usuario")
+    private PropostaEntity proposta;
+}
